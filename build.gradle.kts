0e6f243eec6dfd46efa7b53b25c2f01a8a057236
David Rico <davricchi@gmail.com>
2025-02-26 08:49:29 +0100

    add mariadb

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,10 @@ dependencies {
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.config.yaml)
+    //implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+    implementation(libs.exposed.dao)
+    implementation(libs.mariadb)
+    implementation(libs.exposedjdbc)
 }
